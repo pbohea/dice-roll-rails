d78968b571d54d34bd4a48f731c644b7d02d8b19
pbohea <pbohea@gmail.com>
2025-02-06 16:59:47 +0000

    actually done

--- app/controllers/roll_controller.rb
+++ app/controllers/roll_controller.rb
@@ -30,13 +30,13 @@ class RollController < ApplicationController
     render({ :template => "templates/1_20"})
   end
 
-  def five_six
+  def five_four
     @rolls = []
     5.times do
-    die = rand(1..6)
+    die = rand(1..4)
     @rolls.push(die)
     end
-    render({ :template => "templates/5_6"})
+    render({ :template => "templates/5_4"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "roll", :action => "home"})
   get("/dice/2/6", { :controller => "roll", :action => "two_six"})
-  get("/dice/2/10", { :controller => "roll", :action => "two_twenty"})
+  get("/dice/2/10", { :controller => "roll", :action => "two_ten"})
   get("/dice/1/20", { :controller => "roll", :action => "one_twenty"})
   get("/dice/5/4", { :controller => "roll", :action => "five_four"})
 end    
